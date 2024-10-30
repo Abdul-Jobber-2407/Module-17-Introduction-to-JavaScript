39ee97fb77d8601f67eaf10360fbec730060d7a6
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-30 08:57:26 +0600

    added to a final push

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-// sjotw gwo sfos jfouw3 fowe goiewu 
+/*
+****************************************************************
+*              MATH NEEDED FOR PROGRAMMING
+****************************************************************
+* 1. Number: +ve, -ve , 0 [50 , -9, 0]
+* 2. interger: 2,88,[odd,even]:2,4,897 
+* 3. float: 5.5, 3.2 
+* 4. Operation:+,-,*,/ 
+* 5. More operation: %,[]
+* 6. percentage : 5%, 50 taka er 10% koto ? 
+* 7. sum: 13 435 64 
+* 8. Avg: 13 435 64 
+* 9. Max/ Min : 13 435 64 
+*10. series: 
+*11. Area:Tringel:0.5*base*height
+*12. 
+ */
