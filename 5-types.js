059b6fb3e1fef7a125a6ef5487605bdf0245447c
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-30 09:10:00 +0600

    added to a final push

---
+++ 5-types.js
@@ -0,0 +1,18 @@
+// String
+var name = "AR Jobber"
+console.log(name);
+
+
+var address ="Adar Killa bandorban with some katabon "
+var country = 'Bangladesh'
+// boolean
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+// number
+var price =400;
+console.log(typeof price);
+
---
+++ 6-naming.js
@@ -0,0 +1,35 @@
+// 1.no keyword in variable name
+var false = 59;
+var var = 50;
+
+// 2. no space or gap in variable name
+var is happy = false;
+var ishappy = false;
+
+// 3. no quote
+var "address"= "kochu khet";
+
+// 4. can not start with a number. but number other than the first letter is allowed
+var money = 45;
+var money = 55;
+var 4money =55;
+
+// 5. name is case sensitive \
+address
+Address
+ADDRESS 
+addRESs 
+ADDress 
+
+// 6.how to write a long variable name
+var my current home address = "Andorkila Bandobon Sundorban kata bon";
+var mycurrenthomeaddress = "Andorkila Bandobon Sundorban kata bon";
+var my_current_home_address = "Andorkila Bandobon Sundorban kata bon";  // snake case
+var myCurrentHomeAddress = "Andorkila Bandobon Sundorban kata bon"; // camel case 
+var MyCurrentHomeAddress = "Andorkila Bandobon Sundorban kata bon"; // pascal case
+
+
+// keyword  are case sensitive
+var name = "AR Jobber"
+VAR name = "AR Jobber"
+Var name ="AR Jobber"
